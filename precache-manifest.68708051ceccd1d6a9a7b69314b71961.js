1ecac7afeecb24dce7d3eec602c7e2cac7273a7a
Nikhil Singh Gautam <nick9124>
2020-08-10 04:14:54 +0530

    Updates

---
+++ precache-manifest.68708051ceccd1d6a9a7b69314b71961.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "163236a711a4e01b4fde6003bbd34cef",
+    "url": "/video-search-app/index.html"
+  },
+  {
+    "revision": "235dab280cff5d886f86",
+    "url": "/video-search-app/static/css/main.0874f1e0.chunk.css"
+  },
+  {
+    "revision": "9ded9b404d327dde08a6",
+    "url": "/video-search-app/static/js/2.1c5da7ed.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/video-search-app/static/js/2.1c5da7ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "235dab280cff5d886f86",
+    "url": "/video-search-app/static/js/main.164ae350.chunk.js"
+  },
+  {
+    "revision": "b2a25549a0dd8ddb7d61",
+    "url": "/video-search-app/static/js/runtime-main.8aef8e62.js"
+  }
+]);
